434871a6b88a2a57d6cc037823adce81bfbe138b
Matthew Brennecke <mbrennecke10@gmail.com>
2018-09-25 18:43:56 -0600

    adding files and folders

---
+++ routes/api-routes.js
@@ -0,0 +1,46 @@
+var axios = require("axios");
+var cheerio = require("cheerio");
+var db = require("../models");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the burgers
+  app.get("/", function(req, res) {
+	db.Burger.findAll({}).then(function(dbBurger) {
+		var hbsObject = {
+		burger_name: dbBurger
+		};
+		console.log(hbsObject);
+    res.render("index", hbsObject);
+	});
+  });
+
+  // POST route for saving a new burger. You can create a burger using the data on req.body
+  app.post("/api/burgers", function(req, res) {
+	 db.Burger.create({
+		 burger_name: req.body.name,
+		 devoured: false
+	  })
+      .then(function(dbBurger) {
+		  console.log(dbBurger);
+        res.json(dbBurger);
+      });
+  });
+
+  // PUT route for updating burgers. The updated burgers will be available in req.body
+  app.put("/api/burgers/:id", function(req, res) {
+	db.Burger.update(req.body
+	,{
+        where: {
+          id: req.params.id
+        }
+      }).then(function(dbBurger) {
+		  console.log(dbBurger);
+		  res.json(dbBurger);
+      });
+  });
+};
--- server.js
+++ server.js
@@ -3,10 +3,7 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-var axios = require("axios");
-var cheerio = require("cheerio");
 
-var db = require("./models");
 
 var PORT = process.env.PORT || 8080;
 
@@ -18,7 +15,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
+
+require("./routes/api-routes.js")(app);
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
