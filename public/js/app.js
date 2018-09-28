d483c08ac8b5ba3846411385bbb3e53b81a92149
Matthew Brennecke <mbrennecke10@gmail.com>
2018-09-27 20:11:07 -0600

    that pretty well works

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
 	$.get("/articles")
 	.then(function(data){
-		console.log(data);
 		if(data.length != 0){
 			$(".no-articles").empty();
 		}
+		data.forEach(function(item){
+			$(".articles").prepend(
+			`<div class="card" >
+				<h5 class="card-header "><a href="${item.link}" class="headline">${item.title}</a><button type="button" class="btn btn-secondary float-right note"  id="${item._id}" data-toggle="modal" data-target="#articleModal">Note</button></h5>
+				
+			  <div class="card-body">
+			  <p class="card-text">${item.summary}</p>
+			  </div>
+			</div>`
+		)})
 	})
 })
 
@@ -17,3 +26,38 @@ $(".scrape").on("click", function(event){
 	})
 })
 
+$(document).on("click", ".note", function(event){
+	$("#articleNote").val("");
+	var thisId = $(this).attr("id");
+	$.ajax({
+		method: "GET",
+		url: "/articles/" + thisId
+	}).then(function(data){
+		$("#articleModalCenter").text(data.title);
+		$("#articleNote").removeAttr("data-id");
+		$("#articleNote").attr("data-id", thisId);
+		if (data.note) {
+        // Place the body of the note in the body textarea
+        $("#articleNote").val(data.note.note);
+      }
+
+	})
+
+});
+
+$("#modalNote").on("click", function(){
+	var thisId = $("#articleNote").attr("data-id");
+	console.log(thisId);
+	$.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      // Value taken from note textarea
+      note: $("#articleNote").val()
+    }
+  }).then(function(data){
+	  $("#articleNote").val("");
+	  console.log(data);
+  })
+
+})
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,7 +9,7 @@ var db = require("../models");
 module.exports = function(app) {
 	
 		app.get("/articles", function(req, res) {
-			db.Article.find({})
+			db.Article.find({}).sort({_id: -1})
 			.then(function(dbArticle) {
 				res.json(dbArticle)
 			})
@@ -55,28 +55,41 @@ module.exports = function(app) {
 	  });
 	});
 
-  // POST route for saving a new burger. You can create a burger using the data on req.body
-  app.post("/api/burgers", function(req, res) {
-	 db.Burger.create({
-		 burger_name: req.body.name,
-		 devoured: false
-	  })
-      .then(function(dbBurger) {
-		  console.log(dbBurger);
-        res.json(dbBurger);
-      });
-  });
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
-  // PUT route for updating burgers. The updated burgers will be available in req.body
-  app.put("/api/burgers/:id", function(req, res) {
-	db.Burger.update(req.body
-	,{
-        where: {
-          id: req.params.id
-        }
-      }).then(function(dbBurger) {
-		  console.log(dbBurger);
-		  res.json(dbBurger);
-      });
-  });
-};
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  console.log(req.body);
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+}
